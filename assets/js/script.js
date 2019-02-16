a5e7e36176b9a037e3b740831259de40403299a6
Dennis <deweis@gmx.ch>
2019-02-16 21:36:40 +0100

    Add delete icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,31 @@
 todos = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6'];
 
 function appendTodo(todo) {
-  $('ul').append(`<li>${todo}</li>`);
-  $('li')
+  $('ul').append(
+    `<li><div class="trash-icon"><i class="far fa-trash-alt"></i></div><div class="todo">${todo}</div></li>`
+  );
+
+  $('li div.todo')
     .last()
     .on('click', function(e) {
       $(this).toggleClass('done');
     });
+
+  $('li').on('mouseenter', function() {
+    $(this)
+      .children(':first')
+      .css('width', '30px')
+      .find('i')
+      .css('opacity', '1');
+  });
+
+  $('li').on('mouseleave', function() {
+    $(this)
+      .children(':first')
+      .css('width', '0')
+      .find('i')
+      .css('opacity', '0');
+  });
 }
 
 // Append the todos to the list
