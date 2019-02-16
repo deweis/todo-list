01e737f0783614b06875d068abb5a03682e197d2
Dennis <deweis@gmx.ch>
2019-02-16 20:40:12 +0100

    Add a new todo from input field

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
 todos = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6'];
 
-// Append the todos to the list
-todos.forEach(x => $('ul').append(`<li>${x}</li>`));
+function appendTodo(todo) {
+  $('ul').append(`<li>${todo}</li>`);
+  $('li')
+    .last()
+    .on('click', function(e) {
+      $(this).toggleClass('done');
+    });
+}
 
-// line-through on click
-$('li').on('click', function(e) {
-  $(this).toggleClass('done');
+// Append the todos to the list
+todos.forEach(x => appendTodo(x));
+
+// Add a new task
+$('input').on('keypress', function(e) {
+  var code = e.keyCode || e.which;
+  if (code === 13) {
+    const todo = $('input').val();
+    todos.push(todo);
+    appendTodo(todo);
+    $('input').val('');
+  }
 });
