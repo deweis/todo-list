a7215d2621ac2a6fe081480d2b030047b7da93e4
Dennis <deweis@gmx.ch>
2019-02-16 12:03:37 +0100

    Refactor appending

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 todos = ['Task 1', 'Task 2', 'Task 3', 'Task 4', 'Task 5', 'Task 6'];
 
-for (let i = 0; i < todos.length; i++) {
-  $('ul').append(`<li>${todos[i]}</li>`);
-}
+// Append the todos to the list
+todos.forEach(x => $('ul').append(`<li>${x}</li>`));
 
 // line-through on click
 $('li').on('click', function(e) {
